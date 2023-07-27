f5ae3052a4755b9fa3f1d4186343f0a74d217bd1
Moe Dev <ddevmoe@gmail.com>
2023-07-27 20:40:57 +0300

    Added dummy config module and a bootstraper module

---
+++ src/bootstraper.py
@@ -0,0 +1,12 @@
+from src import config
+from src.loaders import LocalFolderLoader
+from src.parser import ConfigParser
+from src.store import ConfigStore
+
+
+def bootstrap() -> ConfigStore:
+    parser = ConfigParser()
+    loader = LocalFolderLoader(parser, config.LOCAL_FOLDER_LOADER_PATH)
+    store = ConfigStore(loader)
+
+    return store
---
+++ src/config.py
@@ -0,0 +1,4 @@
+import os
+
+
+LOCAL_FOLDER_LOADER_PATH = os.environ.get('LOCAL_CONFIG_FOLDER_PATH', './configs')
