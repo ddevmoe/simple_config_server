dab5101abf81d94111115c47b615aa932ee20e3d
Moe Dev <ddevmoe@gmail.com>
2023-07-27 20:41:17 +0300

    Added web API server using fastapi

---
+++ app.py
@@ -0,0 +1,50 @@
+import uvicorn
+from fastapi import FastAPI, responses, Request, status
+
+from src.router import router
+from src.common import errors
+
+
+app = FastAPI()
+app.include_router(router)
+
+
+#region Custom Error Handling
+
+@app.exception_handler(errors.ConfigNotFoundError)
+async def handle_config_not_found_error(_request: Request, error: errors.ConfigNotFoundError):
+    return responses.JSONResponse(
+        {
+            'name': error.name,
+            'message': error.message,
+            'detailed_message': str(error),
+            'status_code': status.HTTP_404_NOT_FOUND,
+        },
+        status_code=status.HTTP_404_NOT_FOUND
+    )
+
+@app.exception_handler(errors.EnvNotFoundError)
+async def handle_config_not_found_error(_request: Request, error: errors.EnvNotFoundError):
+    return responses.JSONResponse(
+        {
+            'name': error.name,
+            'env': error.env,
+            'message': error.message,
+            'detailed_message': str(error),
+            'status_code': status.HTTP_404_NOT_FOUND,
+        },
+        status_code=status.HTTP_404_NOT_FOUND
+    )
+
+#endregion
+
+
+@app.get('/', include_in_schema=False)
+async def root_redirect():
+    return responses.RedirectResponse('/docs')
+
+
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8200)
---
+++ src/router.py
@@ -0,0 +1,40 @@
+from fastapi import APIRouter, responses, status
+
+from src.bootstraper import bootstrap
+from src.store import ConfigStore
+
+
+store: ConfigStore = None
+
+
+router = APIRouter()
+
+
+@router.on_event('startup')
+async def initialize():
+    global store
+    store = bootstrap()
+    await store.refresh()
+
+
+@router.get('/config/{name}/{env}')
+async def get_config(name: str, env: str):
+    config = await store.get_config(name, env)
+    return responses.JSONResponse(config)
+
+
+@router.post('/reload')
+async def reload_config(name: str):
+    await store.reload(name)
+    return responses.JSONResponse(
+        {
+            'message': 'Loaded successcully',
+            'status_code': status.HTTP_201_CREATED,
+        },
+        status_code=status.HTTP_201_CREATED
+    )
+
+
+@router.post('/refresh')
+async def refresh_config_store():
+    await store.refresh()
