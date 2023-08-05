03b2b4fae0cfb383ea334a50a4227c4286f4ddd7
Moe Dev <ddevmoe@gmail.com>
2023-08-05 17:54:01 +0300

    Added 'SCS' prefix requirement for environment variables

--- src/common/config.py
+++ src/common/config.py
@@ -1,5 +1,14 @@
 import os
 
 
-HTTP_PORT = int(os.environ.get('HTTP_PORT', 8080))
-LOCAL_FOLDER_LOADER_PATH = os.environ.get('LOCAL_CONFIG_FOLDER_PATH', './configs')
+_PREFIX = 'SCS'
+
+
+def resolve_config_value(name: str, default=None) -> str | None:
+    full_name = f'{_PREFIX}_{name}'
+    value = os.environ.get(full_name, default)
+    return value
+
+
+HTTP_PORT = int(resolve_config_value('HTTP_PORT', 8080))
+LOCAL_FOLDER_LOADER_PATH = resolve_config_value('LOCAL_CONFIG_FOLDER_PATH', './configs')
