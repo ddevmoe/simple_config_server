b659d07645113d7a9be7958b0bde8bc3278d2a64
Moe Dev <ddevmoe@gmail.com>
2024-09-18 18:09:15 +0300

    parser and reference_resolver modules now attach generate Problems instead of raising errors

--- src/parser.py
+++ src/parser.py
@@ -1,37 +1,83 @@
 from collections import defaultdict
 from copy import deepcopy
-from typing import Iterable
+from typing import Annotated, Iterable
 
-from src import merger
-from src import reference_resolver
-from src.common.models import Config, EnvConfig, UnparsedConfig
+from pydantic import BaseModel, Field, ValidationError
+
+from src import merger, reference_resolver
+from src.common import config
+from src.common.models import (
+    Config,
+    Location,
+    Problem,
+    EnvConfig,
+    UnparsedConfig,
+)
+
+
+class _ShardSchema(BaseModel):
+    envs: Annotated[
+        list[str],
+        Field(pattern=config.VALID_ENV_PATTERN),
+    ]
+    content: dict
+
+
+class _PreparsedConfig(BaseModel):
+    default: dict
+    shards: list[_ShardSchema]
+
+
+class ConfigParseResult(BaseModel):
+    config: Config | None
+    problems: list[Problem]
 
 
 class ConfigParser:
+    def _parse_config_model_problems(self, validation_error: ValidationError) -> list[Problem]:
+        errors = validation_error.errors(include_url=False)
+
+        problems: list[Problem] = []
+        for error in errors:
+            location = Location([str(key) for key in error['loc']])
+            problem = Problem(message=error["msg"], location=location)
+            problems.append(problem)
+
+        return problems
+
     def _generate_config(self, unparsed_config: UnparsedConfig) -> Config:
         """
         Convert `UnparsedConfig` instance to `Config`
         """
 
-        defaults: dict = unparsed_config.content.get('default', {})
-        shards: list[dict] = unparsed_config.content.get('shards', [])
+        try:
+            preparsed_config = _PreparsedConfig(**unparsed_config.content)
+        except ValidationError as validation_error:
+            problems = self._parse_config_model_problems(validation_error)
+            default_env = EnvConfig(name=unparsed_config.name, env='default', content={}, problems=[])
+            return Config(name=unparsed_config.name, default_env=default_env, envs={}, problems=problems)
 
-        content_by_env: dict[str, dict] = defaultdict(lambda: deepcopy(defaults))
-        content_by_env['default'] = defaults
+        content_by_env: dict[str, dict] = defaultdict(lambda: deepcopy(preparsed_config.default))
 
-        for shard in shards:
-            shard_envs = shard['envs']
-            shard_content = shard['content']
-            for env in shard_envs:
-                merged = merger.merge(content_by_env[env], shard_content)
-                content_by_env[env] = merged
+        problems_by_env: dict[str, list[Problem]] = defaultdict(list)
+        for shard_index, shard in enumerate(preparsed_config.shards):
+            for env in shard.envs:
+                try:
+                    merged = merger.merge(content_by_env[env], shard.content)
+                    content_by_env[env] = merged
+                except merger.MergeUnequalTypesError as merge_error:
+                    # Append location of the merge error in the unparsed config (including shard index)
+                    location = Location(['shards', str(shard_index), *merge_error.path])
+                    problem = Problem(message=str(merge_error), location=location)
+                    problems_by_env[env].append(problem)
 
-        built_configs: dict[str, EnvConfig] = {}
-        for env, content in content_by_env.items():
-            env_config = EnvConfig(unparsed_config.name, env, content)
-            built_configs[env] = env_config
+        built_env_configs = {
+            env: EnvConfig(name=unparsed_config.name, env=env, content=content, problems=problems_by_env[env])
+            for env, content in content_by_env.items()
+        }
 
-        parsed_config = Config(unparsed_config.name, built_configs)
+        default_env = EnvConfig(name=unparsed_config.name, env='default', content=preparsed_config.default, problems=[])
+        parsed_config = Config(name=unparsed_config.name, default_env=default_env, envs=built_env_configs, problems=[])
         return parsed_config
 
     def parse_configs(self, unparsed_configs: list[UnparsedConfig], existing_configs: Iterable[Config]) -> list[Config]:
@@ -50,7 +96,7 @@ class ConfigParser:
         resolved_configs = reference_resolver.resolve_references(current_configs)
         return resolved_configs
 
-    def parse_config(self, unparsed_config: UnparsedConfig, existing_configs: Iterable[Config]):
+    def parse_config(self, unparsed_config: UnparsedConfig, existing_configs: Iterable[Config]) -> Config:
         """
         Converts an `UnparsedConfig` instance to a `Config` instance and resolves
         references to other config files
