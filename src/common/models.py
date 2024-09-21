dca92a4499cc1309144264a01f02469e5644ab93
Moe Dev <ddevmoe@gmail.com>
2024-09-21 20:49:26 +0300

    Added informative repr to config models

--- src/common/models.py
+++ src/common/models.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from typing import Iterable
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 @dataclass
@@ -36,27 +36,55 @@ class UnparsedConfig:
 
 
 class EnvConfig(BaseModel):
-    """Represents a complete configuration of specific value"""
+    """Represents a complete configuration of specific env"""
+
     name: str
     env: str
     content: dict
-    problems: list[Problem]
+    problems: list[Problem] = Field(default_factory=list)
 
     @property
     def has_problems(self) -> bool:
         return bool(self.problems)
+
+    def __repr__(self) -> str:
+        result = f'{self.name}[{self.env}]'
+
+        if self.has_problems:
+            result += f'!({len(self.problems)})'
+
+        return result
 
 
 class Config(BaseModel):
     """Holds all environments of a config"""
+
     name: str
     default_env: EnvConfig
     envs: dict[str, EnvConfig]
-    problems: list[Problem]
+    problems: list[Problem] = Field(default_factory=list)
 
     @property
     def has_problems(self) -> bool:
         return bool(self.problems)
+
+    def __repr__(self) -> str:
+        result = self.name
+
+        env_reprs = [
+            (
+                f'{config_env.env}!({len(config_env.problems)})'
+                if config_env.has_problems
+                else config_env.env
+            )
+            for config_env in [self.default_env, *self.envs.values()]
+        ]
+
+        result = f'{self.name}[{",".join(env_reprs)}]'
+        if self.has_problems:
+            result += f'!({len(self.problems)})'
+
+        return result
 
     def get_env(self, env: str) -> EnvConfig | None:
         return self.envs.get(env)
